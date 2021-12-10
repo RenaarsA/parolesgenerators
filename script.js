a0dadbfba06fc297132ade472017d593490cf340
RenaarsA <DogieLV@Gmail.com>
2021-12-10 15:12:25 +0200

    fix

--- script.js
+++ script.js
@@ -1,36 +1,39 @@
-let variantuSkaits;
+let variantuSkaits=5;
 //let ciparuSkaits;
 //let papildusVards;
 const vardi1=['jautrs' , 'priecīgs', 'smieklīga','kautrīgs','gara']
 const vardi2=['koks','akmens','lapsa','zilonis','žirafe']
 const vardi3=['dejo','smejas','gaudo','lido','ēd']
-let tabulasRindas= document.querySelector('.rindas');
+let rindas= document.querySelector('.rindas');
 
 function randFunc(maxCipars){
-    let random1=Math.floor(math.random() * maxCipars);
+    let random1=Math.floor(Math.random() * maxCipars);
     return random1;
 }
 
 function generet(){
     
-    tabulasRindas.innerHTML =``;
+    rindas.innerHTML =` `;
 
 for (let i = 0 ;i < variantuSkaits; i++) {
- console.log(parole);
-    let parole=vardi1[0]+vardi2[0]+vardi3[0];
+ 
+    
+    
+    
     let rand1 = randFunc(vardi1.length);
     let rand2 = randFunc(vardi2.length);
     let rand3 = randFunc(vardi3.length);
 
-    let parole1 = vardi1[rand1];
-    let parole2 = vardi2[rand2];
-    let parole3 = vardi3[rand3];
+  
 
-    parole = parole1 + parole2+parole3;
-
-    tabulasRindas.innerHTML +=`
+let parole=vardi1[rand1]+vardi2[rand2]+vardi3[rand3];
+console.log(parole);
+    // parole = parole1+parole2+parole3;
+   
+    rindas.innerHTML +=`
     <tr>
     <td>${i+1}</td>
-    >td>${parole}</td>
+    <td>${parole}</td>
     </tr>`
-}}
+}} 
+// <button class="button" onclick="izlozet()">Ģenerēt</button>
